f0780d27ec4340430ce8aa6efd806ca39581c8bb
Olga Zdobnova <zdobnova@haulmont.com>
2020-09-30 14:59:39 +0400

    Changed gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    id("org.springframework.boot") version "2.3.3.RELEASE" apply false
+    id("io.spring.dependency-management") version "1.0.10.RELEASE" apply false
+    kotlin("jvm") version "1.3.72" apply false
+    kotlin("plugin.spring") version "1.3.72" apply false
+}
